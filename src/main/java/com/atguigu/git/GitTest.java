3e3c5bd3a925ba9bc7f1db40e9841c4b9b021b13
Lxk <Lxk199266@163.com>
2021-09-11 20:42:07 +0800

    master commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("HelloGit");
         System.out.println("HelloGit2");
         System.out.println("HelloGit3");
+        System.out.println("HelloGit5");
     }
 }
